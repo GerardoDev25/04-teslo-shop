ff8ef5b5d3ea331d370aa4d3ef65b77e6499ebf5
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-06 10:10:32 -0400

    012 Get y Delete TypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -29,20 +30,32 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOne({ where: { id } });
+
+    if (!product) {
+      throw new NotFoundException(`product with id: ${id} not found`);
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    try {
+      await this.findOne(id);
+      await this.productRepository.delete(id);
+      return { ok: true };
+    } catch (error) {
+      this.handleDbException(error);
+    }
   }
 
   private handleDbException(error: any) {
