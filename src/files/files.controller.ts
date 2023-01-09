d13a95f7419e8a5e14be8d7d2e1344a3a31e594a
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-09 10:05:15 -0400

    006 Guardar imagen en filesystem

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,6 +6,7 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter } from './helpers/filefilter.helper';
 
@@ -14,7 +15,13 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter,
+      // limits: { fileSize: 1000 },
+      storage: diskStorage({ destination: './static/uploads' }),
+    }),
+  )
   uploadedProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('File is empty');
