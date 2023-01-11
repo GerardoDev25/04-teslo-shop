79965cbf8d31d1da64b2d916a1215adf184a22e0
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-11 08:59:40 -0400

    008 Nest Authentication - Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthController } from './auth.controller';
@@ -6,7 +8,14 @@ import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule],
