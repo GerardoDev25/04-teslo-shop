9e5c817cebd397da6114eb6398b678e47f2e5ead
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-12 10:38:14 -0400

    014 Tarea Cambiar email por id en el Payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
       await this.userRepository.save(user);
       delete user.password;
 
-      return { ...user, token: this.getJwToken({ email: user.email }) };
+      return { ...user, token: this.getJwToken({ id: user.id }) };
     } catch (error) {
       this.handleDbException(error);
     }
@@ -44,7 +44,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true },
+      select: { email: true, password: true, id: true },
     });
 
     if (!user) {
@@ -55,7 +55,7 @@ export class AuthService {
       throw new UnauthorizedException('credentials are not valid');
     }
 
-    return { ...user, token: this.getJwToken({ email: user.email }) };
+    return { ...user, token: this.getJwToken({ id: user.id }) };
   }
 
   private handleDbException(error: any): never {
